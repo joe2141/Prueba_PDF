ca308acb99f94615913293988bcd17dd0314ec75
Joel <elsuperjoy@gmail.com>
2023-08-08 16:50:30 -0600

    primer PDF

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { jsPDF } from "jspdf";
 
-function App() {
+async function App() {
+  async function fetchData() {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+  
+    try {
+      const response = await fetch("http://localhost:3000/alumnos", requestOptions);
+      const result = await response.json(); // Parse response as JSON
+      return result;
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+  
+  const datos = await fetchData(); // Wait for the array of data
+  
+  if (datos && datos.length > 0) {
+    const primerDato = datos[0];
+  
+    console.log(primerDato);
+    console.log(primerDato.nombre);
+  
+    const doc = new jsPDF();
+    doc.text(`a nombre de ${primerDato.nombre}`, 10, 10);
+    // Agregar una imagen
+    const imgData = "data:image/png;base64,/9j/4AAQSk..."; // Reemplaza esto con tu cadena base64 de la imagen
+    doc.addImage(imgData, "JPEG", 10, 20, 50, 50); // Ajusta las coordenadas y el tamaño según sea necesario
+    doc.save("a4.pdf");
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Helllo</h1>
     </div>
   );
 }
